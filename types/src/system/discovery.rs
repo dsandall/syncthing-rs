74173f90673bf56eb598cf26f90a8a554fbbb957
Jayce Fayne <jayce.fayne@mailbox.org>
2025-09-21 10:46:00 +0200

    parse Uri in `Discovery`

--- types/src/system/discovery.rs
+++ types/src/system/discovery.rs
@@ -1,13 +1,12 @@
 use crate::DeviceID;
+use http::Uri;
 use serde::Deserialize;
 use std::collections::HashMap;
 
-//FIXME: verify that this is the device id
-pub type Addr = String;
-
 #[derive(Debug, Deserialize)]
 pub struct Addresses {
-    pub addresses: Vec<Addr>,
+    #[serde(with = " http_serde_ext::uri::vec")]
+    pub addresses: Vec<Uri>,
 }
 
 pub type Discovery = HashMap<DeviceID, Addresses>;
